b1d8171c1ae03c4ab728bbde49fe12af6e9ec721
mustapha bouhou <57944084+Mubenyo@users.noreply.github.com>
2023-12-14 00:31:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main(int ac, char **av)
 		}
 		details->readfd = fd;
 	}
-	encrease_env_list(details);
+	increase_env_list(details);
 	read_history(details);
 	hsh(details, av);
 	return (EXIT_SUCCESS);
