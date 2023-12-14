5664586a38f3886efe12bb64a1fd037169884333
mustapha bouhou <57944084+Mubenyo@users.noreply.github.com>
2023-12-14 12:25:50 +0100

    Update str_function2.c

--- str_function2.c
+++ str_function2.c
@@ -46,11 +46,11 @@ char *_strdup(const char *str)
 }
 
 /**
- *_puts - prints an input string
- *@str: the string to be printed
- *
- * Return: Nothing
- */
+*_puts - prints an input string
+*@str: the string to be printed
+*
+* Return: Nothing
+*/
 void _puts(char *str)
 {
 	int i = 0;
@@ -65,12 +65,12 @@ void _puts(char *str)
 }
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
 	static int i;
