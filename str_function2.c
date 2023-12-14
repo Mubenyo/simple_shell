92eee1e6b23861a6b184afedd92a3abaf7f6d9c5
mustapha bouhou <57944084+Mubenyo@users.noreply.github.com>
2023-12-14 11:27:03 +0100

    Update str_function2.c

--- str_function2.c
+++ str_function2.c
@@ -1,6 +1,28 @@
 #include "shell.h"
 
 /**
+ * _strcpy - copies a string
+ * @destination: the destination
+ * @source: the source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *destination, char *source)
+{
+	int index = 0;
+
+	if (destination == source || source == 0)
+		return (destination);
+	while (source[index])
+	{
+		destination[index] = source[index];
+		index++;
+	}
+	destination[index] = 0;
+	return (destination);
+}
+
+/**
  * _strdup - duplicates a string
  * @str: the string to duplicate
  *
@@ -52,61 +74,4 @@ void _puts(char *str)
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-
-/**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
-}
-
-/**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
 }
