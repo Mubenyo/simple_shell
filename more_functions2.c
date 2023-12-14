c1cfb94d025d49df1df9f063170e7843e3617856
mustapha bouhou <57944084+Mubenyo@users.noreply.github.com>
2023-12-14 13:15:21 +0100

    Update more_functions2.c

--- more_functions2.c
+++ more_functions2.c
@@ -2,7 +2,7 @@
 
 /**
  * print_error - prints an error message
- * @info: the parameter & return info struct
+ * @details: the parameter & return info struct
  * @estr: string containing specified error type
  * Return: 0 if no numbers in string, converted number otherwise
  *        -1 on error
