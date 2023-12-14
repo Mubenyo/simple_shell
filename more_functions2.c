106de2b739bd746d4df0af0de03e87ec8e4a219b
mustapha bouhou <57944084+Mubenyo@users.noreply.github.com>
2023-12-14 12:22:27 +0100

    Update and rename more_functions2 to more_functions2.c

--- more_functions2.c
+++ more_functions2.c
@@ -1,47 +1,19 @@
 #include "shell.h"
 
 /**
- * err_str_to_int - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
- */
-int err_str_to_int(char *s)
-{
-	int i = 0;
-	unsigned long int result = 0;
-
-	if (*s == '+')
-		s++;
-	for (i = 0;  s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	return (result);
-}
-
-/**
  * print_error - prints an error message
  * @info: the parameter & return info struct
  * @estr: string containing specified error type
  * Return: 0 if no numbers in string, converted number otherwise
  *        -1 on error
  */
-void print_error(info_t *info, char *estr)
+void print_error(details_t *details, char *estr)
 {
-	_eputs(info->fname);
+	_eputs(details->fname);
 	_eputs(": ");
-	print_d(info->line_count, STDERR_FILENO);
+	print_decimal(details->line_count, STDERR_FILENO);
 	_eputs(": ");
-	_eputs(info->argv[0]);
+	_eputs(details->argv[0]);
 	_eputs(": ");
 	_eputs(estr);
 }
@@ -86,7 +58,7 @@ int print_decimal(int input, int fd)
 }
 
 /**
- * convert_num - converter function, a clone of itoa
+ * convert_num - converter function, a clone of str_to_int
  * @num: number
  * @base: base
  * @flags: argument flags
